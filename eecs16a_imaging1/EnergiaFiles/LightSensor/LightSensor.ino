2b82174733e7c11f6e378c5807ea6bb4c7435bf7
Leyla Kabuli <lakabuli@berkeley.edu>
2020-07-02 17:42:13 -0700

    fixed last 9600

--- eecs16a_imaging1/EnergiaFiles/LightSensor/LightSensor.ino
+++ eecs16a_imaging1/EnergiaFiles/LightSensor/LightSensor.ino
@@ -6,8 +6,8 @@
 
 // the setup routine runs once when you press reset:
 void setup() {
-  // initialize serial communication at 9600 bits per second:
-  Serial.begin(9600);
+  // initialize serial communication at 115200 bits per second:
+  Serial.begin(115200);
 }
 
 // the loop routine runs over and over again forever:
